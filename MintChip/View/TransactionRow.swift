35da10e1af5011be39d56884a87712f0bf6e17d4
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-05-04 07:32:45 +0530

    Transaction Row

---
+++ MintChip/View/TransactionRow.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionRow.swift
+//  MintChip
+//
+//  Created by Mrinmoy Borah on 04/05/24.
+//
+
+import SwiftUI
+
+struct TransactionRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionRow()
+}
