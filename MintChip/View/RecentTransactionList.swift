acaf867d46213b08924f0837d1eedc04e2843f1c
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-08-27 23:48:14 +0530

    Transactions by month

---
+++ MintChip/View/RecentTransactionList.swift
@@ -0,0 +1,18 @@
+//
+//  RecentTransactionList.swift
+//  MintChip
+//
+//  Created by Mrinmoy Borah on 27/08/24.
+//
+
+import SwiftUI
+
+struct RecentTransactionList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecentTransactionList()
+}
---
+++ MintChip/View/TransactionList.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionList.swift
+//  MintChip
+//
+//  Created by Mrinmoy Borah on 27/08/24.
+//
+
+import SwiftUI
+
+struct TransactionList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionList()
+}
